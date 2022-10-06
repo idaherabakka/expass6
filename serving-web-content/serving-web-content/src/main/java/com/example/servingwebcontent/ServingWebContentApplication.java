80bf582690cdadd3c0ea56e06bb0066ca5efd744
idaher <xek010@uib.no>
2022-10-06 21:32:32 +0200

    upload

--- serving-web-content/serving-web-content/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ serving-web-content/serving-web-content/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServingWebContentApplication {
 
+	//test comment
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServingWebContentApplication.class, args);
 	}
